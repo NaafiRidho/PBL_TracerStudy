f62b98bef20ad522c001b2c959fd2273820b3663
boby <aww3992@gmail.com>
2025-06-06 12:49:30 +0700

    add route untuk menjalankan built-in PHP web server

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 | Bisa ditambahkan middleware 'auth' dan 'role:admin' jika perlu.
 |
 */
-
+Route::get('/', [LoginController::class, 'showLoginForm']);
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
